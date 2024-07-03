d9fb294e46a0c37bebc60186c9f7c12078f1f41f
iDamina <daminailiya@gmail.com>
2024-07-03 02:58:20 +0100

    solution to 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+A simple flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Summary
+
+    Returns:
+            type: description
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strick_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages
+
+    Returns:
+            type: description
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    # determine the best match for the supported languages
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Summary
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
